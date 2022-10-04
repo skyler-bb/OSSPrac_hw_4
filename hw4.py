4517320f7f22b66eb5133bab0f3ce76ab59bb944
skyler-bb <yo.oncc1701@gmail.com>
2022-10-04 23:58:52 +0900

    10.04.22 hw5 no.3

--- hw4.py
+++ hw4.py
@@ -1,8 +1,10 @@
 name = input("이름을 입력하세요: ")
 ss_no = input("학번을 입력하세요: ")
 dprt = input("학과를 입력하세요: ")
+univ = input("학교를 입력하세요: ")
 
 print("<출력>")
 print("이름: ", name)
 print("학번: ", ss_no)
 print("학과: ", dprt)
+print("학교: ",univ)
